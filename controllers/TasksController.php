85553ae0ad522638ddcf1eb9e2f96d46a089d2cc
nerodemiurgo <nerodemiurgo@gmail.com>
2022-05-05 22:03:42 +0300

    добавлена фильтрация на страницу заданий

--- controllers/TasksController.php
+++ controllers/TasksController.php
@@ -2,12 +2,16 @@
 
 namespace app\controllers;
 
+use Yii;
 use yii\web\Controller;
 use app\models\Task;
+use app\models\Category;
+use app\models\forms\TaskSearchForm;
+use nerodemiurgo\services\TaskService;
+use yii\helpers\ArrayHelper;
 
 class TasksController extends Controller
 {
-
     public function getTasks()
     {
         return Task::find()
@@ -19,8 +23,27 @@ class TasksController extends Controller
 
     public function actionIndex()
     {
-        $tasks = $this->getTasks();
-        return $this->render('index', compact("tasks"));
-    }
+        $taskSearchForm = new TaskSearchForm();
 
+        $request = Yii::$app->request;
+        if ($request->isGet && !empty($request->queryParams['TaskSearchForm'])) {
+            $newTasks = $taskSearchForm->filterTasks($request->queryParams['TaskSearchForm']);
+        } else {
+            $newTasks = $this->getTasks();
+        }
+        $categories = Category::find()->indexBy('id')->all();
+        $taskService = new TaskService($newTasks, $categories);
+        $listTasks = $taskService->getListTasks();
+        $listPeriods = $taskService->getListPeriods();
+
+        return $this->render(
+            'index',
+            [
+                'tasks' => $listTasks,
+                'categories' => $categories,
+                'taskSearchForm' => $taskSearchForm,
+                'listPeriods' => $listPeriods,
+            ]
+        );
+    }
 }
